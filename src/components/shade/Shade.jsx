235cee075a3813827c025cae5183ce0c33797abc
Codimaniac <simplyanthony007@gmail.com>
2025-10-16 15:16:23 +0100

    Fix: fixed the shade component

--- src/components/shade/Shade.jsx
+++ src/components/shade/Shade.jsx
@@ -1,6 +1,6 @@
 const Shade = ({className}) => {
   return (
-    <div className={`absolute translate-[-50%] blur-3xl opacity-[15%] gradient-rad-bg rounded-[50%] ${className}`}></div>
+    <div className={`hidden z-2 absolute translate-[-50%] blur-3xl opacity-[15%] gradient-rad-bg rounded-[50%] lg:block ${className}`}></div>
   )
 }
 
