385dc764bb2bdb9b4febbe06774dc179fe0c5372
Codimaniac <simplyanthony007@gmail.com>
2025-10-18 09:31:33 +0100

    Fix: adjusted margin for mobile view of NFT carousel

--- src/containers/nfts-carousel/NFTsCarousel.jsx
+++ src/containers/nfts-carousel/NFTsCarousel.jsx
@@ -7,7 +7,7 @@ const NFTsCarousel = ({items, itemsPerSlide}) => {
   
      return (
       <>
-        <div className="flex flex-col justify-center gap-x-4 gap-y-32 mt-2 lg:flex-row lg:flex-wrap">
+        <div className="flex flex-col gap-x-4 gap-y-32 mt-2 lg:flex-row lg:flex-wrap">
           {
               currentSlide.map((item) => {
                   return (
--- src/pages/CreatorProfile.jsx
+++ src/pages/CreatorProfile.jsx
@@ -11,20 +11,20 @@ const CreatorProfile = () => {
 
     return (
         <div className="flex flex-col gap-4 items-center section__margin">
-            <div className="relative flex bg-[#16192a] border-1 border-[#2e3150] w-full h-50">
+            <div className="relative flex bg-[#16192a] border-1 border-[#2e3150] w-full h-50 mb-25 lg:mb-0">
                 <div className="absolute top-[110%] left-1/2 translate-[-50%] flex flex-col items-center gap-2">
                     <img src={Creator?.creator_pfp} alt={Creator?.creator_name} className="w-36 h-36 rounded-full shadow-lg shadow-[#2e3150]" />
                     <span className="font-bold">{Creator?.creator_name}</span>
                     <span className="text-xs font-light">@{Creator?.creator_name}</span>
                 </div>
             </div>
-            <ul className="hidden flex-row font-light text-sm mt-40 sm:hidden md:gap-8 lg:flex xl:gap-14">
+            <ul className="hidden flex-row font-light text-sm mt-35 sm:hidden md:gap-8 lg:flex xl:gap-14">
                 <li><Link to="/" className="no-underline hover:text-[var(--accent-color)]">Product</Link></li>
                 <li><Link to="/marketplace" className="no-underline hover:text-[var(--accent-color)]">About</Link></li>
                 <li><a href="#artist" className="no-underline hover:text-[var(--accent-color)]">Policies</a></li>
                 <li><a href="#community" className="no-underline hover:text-[var(--accent-color)]">Review</a></li>
             </ul>
-            <div className="flex justify-between w-full mt-12 font-light">
+            <div className="flex justify-between w-full mt-8 font-light">
                 <span>Showing all 9 results</span>
                 <span className="flex items-center gap-2">Sort by latest <IoMdArrowDropdown /> </span>
             </div>
