98fe1e4dc5afb6a5c612a06ab08c0cd465ce7c88
Codimaniac <simplyanthony007@gmail.com>
2025-10-21 16:58:46 +0100

    Feat: Added Firebase node package

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 const provider = new GoogleAuthProvider();
 export const errorMessage = ""
 
