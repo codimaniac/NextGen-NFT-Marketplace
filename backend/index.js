59944541ea35e4e6165299d73c860f305521acea
Codimaniac <simplyanthony007@gmail.com>
2025-10-27 10:54:01 +0100

    Fix: Fixed PORT for deployment on Render

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const url = require('url')
 const data = require('../data/db.json')
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 const app = http.createServer((req, res) => {
     const parsedURL = url.parse(req.url, true)
@@ -79,5 +79,5 @@ const app = http.createServer((req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
